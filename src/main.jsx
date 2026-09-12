6996af9e8c9de26a3403fab54e3e53966c27dacd
gelfrix07-source <gelfrix07@gmail.com>
2026-09-12 20:08:30 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,19 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import { SettingsProvider } from './contexts/SettingsContext.jsx'
+import { CartProvider } from './contexts/CartContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <SettingsProvider>
-          <App />
-        </SettingsProvider>
-      </AuthProvider>
+  <SettingsProvider>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </SettingsProvider>
+</AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
